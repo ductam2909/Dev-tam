25133644ff76c297c9a961afd24b88faa58ff3e5
ductam2909 <66300515+ductam2909@users.noreply.github.com>
2023-06-24 22:54:05 +0700

    create dev

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import * as S from './styled'
+import { Col, Input, Row } from 'antd';
 
 export default function Home() {
-  return <div>Home</div>;
+  return <S.Wraper>
+    <Row justify={'center'} align={'middle'} gutter={[16,16]}>
+      <Col md={7}><h2>restos</h2></Col>
+      <Col md={1}><h4>Home</h4></Col>
+      <Col md={1}><h4>about</h4></Col>
+      <Col md={1}><h4>contasct</h4></Col>
+      <Col md={1}><h4>contact</h4></Col>
+      <Col md={6} offset={4}><Input/></Col>
+    </Row>
+  </S.Wraper>;
 }
